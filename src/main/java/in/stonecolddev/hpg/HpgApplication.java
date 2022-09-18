e71759ef013c958386ba5ac29c4ee1150d8d20cc
Devin Austin <devin.austin@gmail.com>
2022-09-18 15:33:54 -0600

    cleanup

--- src/main/java/in/stonecolddev/hpg/HpgApplication.java
+++ src/main/java/in/stonecolddev/hpg/HpgApplication.java
@@ -3,20 +3,14 @@ package in.stonecolddev.hpg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 @EnableScheduling
 @ConfigurationPropertiesScan
-public class HpgApplication { //extends SpringBootServletInitializer {
+public class HpgApplication {
 
-	//@Override
-	//protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	//	return application.sources(HpgApplication.class);
-	//}
 	public static void main(String[] args) {
 		SpringApplication.run(HpgApplication.class, args);
 	}
