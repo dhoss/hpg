ce275e3044404c19ea450fb6e9d7439c3520f2f6
Devin Austin <devin.austin@gmail.com>
2024-03-21 14:47:20 -0600

    updated test categories, fleshed out setup_database.sql

--- src/main/resources/db/setup_database.sql
+++ src/main/resources/db/setup_database.sql
@@ -1,5 +1,14 @@
+drop database hpg;
 create database hpg;
 
+drop owned by hpg;
+drop user hpg;
 create user hpg with password 'hpg';
 
-grant all privileges on database "hpg" to hpg;
+grant all privileges on database hpg to hpg;
+
+grant all on schema public to hpg;
+
+grant usage, create on schema public to hpg;
+
+alter database hpg owner to hpg;
