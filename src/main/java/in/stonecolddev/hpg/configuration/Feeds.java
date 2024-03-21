bb0c5678a4863d156be2a6a3244d485b3e06c744
Devin Austin <devin.austin@gmail.com>
2024-03-21 15:42:22 -0600

    SBM: applied recipe 'sbu30-remove-construtor-binding'

--- src/main/java/in/stonecolddev/hpg/configuration/Feeds.java
+++ src/main/java/in/stonecolddev/hpg/configuration/Feeds.java
@@ -2,14 +2,12 @@ package in.stonecolddev.hpg.configuration;
 
 import io.soabase.recordbuilder.core.RecordBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Profile;
 
 import java.time.Duration;
 import java.util.List;
 
 @ConfigurationProperties(prefix = "feeds")
-@ConstructorBinding
 @Profile({"local", "unit-test", "it-test", "dev", "prod"})
 public record Feeds(
     Duration cacheExpire,
