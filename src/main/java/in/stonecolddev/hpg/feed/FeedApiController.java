d5cea8d36dbc41ff696b3e33335ced89b2d5ed2b
Devin Austin <devin.austin@gmail.com>
2024-04-15 15:22:29 -0600

    initial 'mark as seen' works, but only for the first button press

--- src/main/java/in/stonecolddev/hpg/feed/FeedApiController.java
+++ src/main/java/in/stonecolddev/hpg/feed/FeedApiController.java
@@ -8,16 +8,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Clock;
+import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class FeedApiController {
 
   private final Logger log = LoggerFactory.getLogger(FeedController.class);
 
+  private final FeedRepository feedRepository;
+
+  private final Clock clock;
+
+  public FeedApiController(
+      FeedRepository feedRepository,
+      Clock clock) {
+
+    this.feedRepository = feedRepository;
+    this.clock = clock;
+  }
+
   @PostMapping("/feed/seen")
   public HttpEntity<List<MarkUrlAsSeen>> markAsSeen(@RequestBody List<MarkUrlAsSeen> urls) {
+    feedRepository.saveAll(
+        urls.stream().map(this::fromMarkUrlAsSeen)
+            .collect(Collectors.toList()));
+
     return ResponseEntity.ok(urls);
+  }
+
+  private FeedUrlHistory fromMarkUrlAsSeen(MarkUrlAsSeen markUrlAsSeen) {
+    return FeedUrlHistoryBuilder.builder()
+        .created(OffsetDateTime.now(clock))
+        .url(markUrlAsSeen.url().toString())
+        .hash(markUrlAsSeen.hash())
+        .build();
   }
 
 }
--- src/main/java/in/stonecolddev/hpg/feed/FeedUrlHistory.java
+++ src/main/java/in/stonecolddev/hpg/feed/FeedUrlHistory.java
@@ -1,11 +1,13 @@
 package in.stonecolddev.hpg.feed;
 
 import io.soabase.recordbuilder.core.RecordBuilder;
+import org.springframework.data.annotation.Id;
 
 import java.time.OffsetDateTime;
 
 @RecordBuilder
 public record FeedUrlHistory(
+    @Id
     int id,
     String url,
     String hash,
